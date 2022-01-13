f9ccd6031b7273fccadc2b1a436e38e365113ef4
Marco Rizzo <marco.rizzo.00@outlook.com>
2022-01-13 23:06:17 +0100

    edit title, add more info about me

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import "./info.css"
 
 const Info = () => {
     return (
         <div id="info" className='portfolio__info section__padding'>
             <div className="portfolio__info-content">
+                <div className='portfolio__info-content-title'>
+                    <h1>About Me</h1>   
+                </div>
+                <div className='portfolio__info-content-text'>
+                    <p>I am a 21 years old IT Engineering student with a great passion for everything that involves a computer. I've recently got more involved in the great world of Open Source Software and I fell in love with it. In my free time I usually like coding or playing video games, mostly racing sims. I'm currently trying to learn ReactJS and Python, while also keeping my focus on improving my Java skills. I'm also a custom ROM maintainer for my mobile device.</p>
+                </div>
+
             </div>
         </div>
     )
