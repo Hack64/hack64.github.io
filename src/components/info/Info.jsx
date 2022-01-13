32b5217e64d56bfcb2ac686ef70076791f01a365
Marco <marco.rizzo.00@outlook.com>
2022-01-13 23:27:27 +0100

    grammar mistakes

--- src/components/info/Info.jsx
+++ src/components/info/Info.jsx
@@ -9,7 +9,7 @@ const Info = () => {
                     <h1>About Me</h1>   
                 </div>
                 <div className='portfolio__info-content-text'>
-                    <p>I am a 21 years old IT Engineering student with a great passion for everything that involves a computer. I've recently got more involved in the great world of Open Source Software and I fell in love with it. In my free time I usually like coding or playing video games, mostly racing sims. I'm currently trying to learn ReactJS and Python, while also keeping my focus on improving my Java skills. I'm also a custom ROM maintainer for my mobile device.</p>
+                    <p>I am a 21 years old IT Engineering student with a great passion for everything that involves a computer. I've recently gotten more involved in the great world of Open Source Software and I fell in love with it. In my free time, I usually like coding or playing video games, mostly racing sims. I'm currently trying to learn ReactJS and Python, while also keeping my focus on improving my Java skills. I'm also a custom ROM maintainer for my mobile device.</p>
                 </div>
 
             </div>
